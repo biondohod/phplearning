b11a52905defeddc4cc174862206b2b1d7d48a5d
biondohod <biondohod@gmail.com>
2024-03-15 15:27:58 +0500

    изучаю пхп

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-text
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">Firstname?</label>
+            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+            <label for="lastname">Lastname?</label>
+            <input id="lastname" type="text" name="lastname" placeholder="Lastname...">
+            <label for="favouritepet">Favourite Pet?</label> <select id="favouritepet" name="favouritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="bird">Bird</option>
+            </select>
+            <button type="submit">Submit</button>
+        </form>
+    </main>
+
+</body>
+
+</html>
